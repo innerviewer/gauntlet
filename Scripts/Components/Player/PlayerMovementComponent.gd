05fc913037d0d94a69735cff4974ca2e335f1ea3
innerviewer <innerviewerr@gmail.com>
2024-10-02 17:20:24 +0200

    (PMC) Fixed trails and added blink cooldown.

--- Scripts/Components/Player/PlayerMovementComponent.gd
+++ Scripts/Components/Player/PlayerMovementComponent.gd
@@ -3,15 +3,18 @@ class_name PlayerMovementComponent
 
 @export var blink_restraint: float = 0.75
 @export var blink_range: int = 400
+@export var blink_cooldown: float = 1.0
 
 @onready var animations_component: AnimationComponent = $"../AnimationsComponent"
 @onready var shaders_component: ShaderComponent = $"../ShadersComponent"
 @onready var sword: Area2D = $"../Sword"
 
 var reveal_blink_range: bool = false
+var can_blink: bool = true
+
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("blink"):
+	if event.is_action_pressed("blink") and can_blink:
 		blink()
 	
 	if event.is_action_pressed("helper_ui"):
@@ -36,18 +39,23 @@ func process_movement(delta: float) -> void:
 	parent.move_and_slide()
 	
 	animations_component.update_direction_animations(input_direction)
-	animations_component.update_facing_direction(input_direction,sword)
+	animations_component.update_facing_direction(input_direction, sword)
+
+func set_can_blink(value: bool) -> void:
+	can_blink = value
 
 func blink() -> void:
+	can_blink = false
+	
+	var start_position: Vector2 = parent.global_position
 	var blink_position: Vector2 = parent.get_global_mouse_position()
-	var start_position: Vector2 = parent.get_global_position()
 	var direction: Vector2 = blink_position - parent.position
 	var distance: float = direction.length()
-	
-	shaders_component.create_trail_effect(start_position, blink_position)
 	
 	if distance > blink_range:
 		direction = direction.normalized() * (blink_range * blink_restraint) 
 		parent.position += direction
 	else:
 		parent.position = blink_position
+
+	shaders_component.create_trail_effect(start_position, parent.global_position, blink_cooldown, set_can_blink)
--- Scripts/Components/Shaders/ShaderComponent.gd
+++ Scripts/Components/Shaders/ShaderComponent.gd
@@ -1,40 +1,39 @@
 extends Node2D
 class_name ShaderComponent
 
+@export var segments: int = 10
+@export var shadow_opacity: float = 0.2
+@export var shadow_color: Color = Color.DARK_GRAY
+@export var shadow_mix_color: float = 1.0
+
 @onready var character_sprite: Sprite2D = $"../Sprite2D"
+
 
 func flash_shader() -> void:
 	character_sprite.material.set("shader_parameter/flash_modifier", 1.0)
 	await get_tree().create_timer(0.1).timeout
 	character_sprite.material.set("shader_parameter/flash_modifier", 0.0)
-
-func create_trail_effect(start_position: Vector2, end_position: Vector2) -> void:
-	var segments: int = 10 # Количество следов
-	var interval: float = 0.1 # Интервал времени между созданием дубликатов
+	
+func create_trail_effect(start_position: Vector2, end_position: Vector2, blink_duration: float, can_blink_callback: Callable) -> void:
+	var trail_sprites: Array = []
+	var interval: float = blink_duration / segments
 	var step: Vector2 = (end_position - start_position) / segments
-	var previous_trail_sprite: Sprite2D = null
-		
-	for i in range(segments):
-		await get_tree().create_timer(interval).timeout # Задержка перед созданием каждого следа
+	
+	for i: int in range(segments):
 		var trail_sprite: Sprite2D = character_sprite.duplicate()
+		get_tree().get_current_scene().add_child(trail_sprite)
+		trail_sprites.append(trail_sprite)
 		
-		if previous_trail_sprite:
-			previous_trail_sprite.queue_free()
-			
-		# Позиция вдоль пути телепортации
-		trail_sprite.position = start_position + step * i - character_sprite.get_parent().position
-		trail_sprite.modulate = Color(1, 1, 1, 1) 
+		trail_sprite.global_position = start_position + (step * i)
+		trail_sprite.modulate = shadow_color
 		
-		var new_material: Material = trail_sprite.material.duplicate()
-		trail_sprite.material = new_material
+		trail_sprite.material = trail_sprite.material.duplicate()
 		
-		character_sprite.get_parent().add_child(trail_sprite)
+		trail_sprite.material.set("shader_parameter/mix_color", shadow_mix_color)
+		trail_sprite.material.set("shader_parameter/opacity", shadow_opacity)
 		
-		trail_sprite.material.set("shader_parameter/mix_color", 1.0 - (i / segments))
-		trail_sprite.material.set("shader_parameter/opacity", 0.5 - (i / segments))
-		
-		previous_trail_sprite = trail_sprite
-		
-	if previous_trail_sprite:
+	for sprite: Sprite2D in trail_sprites:
 		await get_tree().create_timer(interval).timeout
-		previous_trail_sprite.queue_free()
+		sprite.queue_free()
+	
+	can_blink_callback.call(true)
